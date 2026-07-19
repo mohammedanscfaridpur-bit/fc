7447e3760c3efdb99e08d4295f0a3c5d26005f50
Mohammeda SC Faridpur <bappee02@gmail.com>
2026-07-19 16:40:06 +0600

    Use Next.js Link

--- src/components/committee/person-card.tsx
+++ src/components/committee/person-card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useLocale, useTranslations } from "next-intl";
-import { Link } from "@/i18n/routing";
+import Link from "next/link";
 import { Card, CardTitle, CardDescription } from "@/components/ui/card";
 
 interface PersonCardProps {
