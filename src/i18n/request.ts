f1a6434c21c89b8683d414db4fa66614c8d8f87d
Mohammeda SC Faridpur <bappee02@gmail.com>
2026-07-19 15:22:40 +0600

    Fix next-intl import

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,10 +1,12 @@
 import { getRequestConfig } from "next-intl/server";
-import { hasLocale } from "next-intl";
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
-  const locale = hasLocale(routing.locales, requested)
+
+  const locale = routing.locales.includes(
+    requested as (typeof routing.locales)[number]
+  )
     ? requested
     : routing.defaultLocale;
 
